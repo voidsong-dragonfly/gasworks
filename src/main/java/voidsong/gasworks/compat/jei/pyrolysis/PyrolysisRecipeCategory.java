e4f18f73226481d50b4198d98267685b0ed915f3
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2025-05-21 01:40:57 -0400

    Update looks & slot draws for JEI Pyrolysis cat

--- src/main/java/voidsong/gasworks/compat/jei/pyrolysis/PyrolysisRecipeCategory.java
+++ src/main/java/voidsong/gasworks/compat/jei/pyrolysis/PyrolysisRecipeCategory.java
@@ -11,6 +11,7 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeHolder;
 import voidsong.gasworks.api.recipe.recipes.PyrolysisRecipe;
 import voidsong.gasworks.common.registry.GSItems;
+import voidsong.gasworks.compat.jei.JEIPlugin;
 import voidsong.gasworks.compat.jei.JEIRecipeTypes;
 import voidsong.gasworks.compat.jei.RecipeCategory;
 
@@ -25,32 +26,38 @@ public class PyrolysisRecipeCategory extends RecipeCategory<PyrolysisRecipe> {
 
     @Override
     public int getWidth() {
-        return 120;
+        return 142;
     }
 
     @Override
     public int getHeight() {
-        return 38;
+        return 22;
     }
 
     @Override
     public void setRecipe(@Nonnull IRecipeLayoutBuilder builder, RecipeHolder<PyrolysisRecipe> recipeHolder, @Nonnull IFocusGroup focuses) {
         PyrolysisRecipe recipe = recipeHolder.value();
-        builder.addSlot(RecipeIngredientRole.INPUT, 1, 17)
+        builder.addSlot(RecipeIngredientRole.INPUT, 1, 3)
             .addItemStacks(Arrays.stream(Ingredient.of(recipe.getWalls()).getItems()).toList());
 
-        builder.addSlot(RecipeIngredientRole.INPUT, 21, 17)
+        builder.addSlot(RecipeIngredientRole.INPUT, 21, 3)
             .addItemStack(recipe.getIngredient());
 
-        builder.addSlot(RecipeIngredientRole.OUTPUT, 85, 17)
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 105, 3)
             .addItemStack(recipe.getResult());
 
-        builder.addSlot(RecipeIngredientRole.OUTPUT, 105, 17)
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 125, 3)
             .addItemStack(recipe.getAsh());
     }
 
     @Override
     public void draw(@Nonnull RecipeHolder<PyrolysisRecipe> recipe, @Nonnull IRecipeSlotsView recipeSlotsView, @Nonnull GuiGraphics graphics, double mouseX, double mouseY) {
+        // Draw the slots
+        JEIPlugin.slotDrawable.draw(graphics, 0, 2);
+        JEIPlugin.slotDrawable.draw(graphics, 20, 2);
+        JEIPlugin.slotDrawable.draw(graphics, 104, 2);
+        JEIPlugin.slotDrawable.draw(graphics, 124, 2);
+        // Draw the moving parts
         /*
         this.flame.draw(graphics, 52, 0);
         this.flameAnimated.draw(graphics, 52, 0);
