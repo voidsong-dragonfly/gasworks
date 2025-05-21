dc63570d79a377ee577fdb1d0f80e9b2b98f9fdf
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2025-05-21 11:50:02 -0400

    Finish up in-world pyrolysis JEI category

--- src/main/java/voidsong/gasworks/compat/jei/pyrolysis/PyrolysisRecipeCategory.java
+++ src/main/java/voidsong/gasworks/compat/jei/pyrolysis/PyrolysisRecipeCategory.java
@@ -2,13 +2,17 @@ package voidsong.gasworks.compat.jei.pyrolysis;
 
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.drawable.IDrawableStatic;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeHolder;
+import voidsong.gasworks.Gasworks;
 import voidsong.gasworks.api.recipe.recipes.PyrolysisRecipe;
 import voidsong.gasworks.common.registry.GSItems;
 import voidsong.gasworks.compat.jei.JEIPlugin;
@@ -19,14 +23,25 @@ import javax.annotation.Nonnull;
 import java.util.Arrays;
 
 public class PyrolysisRecipeCategory extends RecipeCategory<PyrolysisRecipe> {
+    private static final ResourceLocation guiElements = Gasworks.rl("textures/gui/pyrolysis.png");
+    private final IDrawableStatic arrow;
+    private final IDrawableAnimated arrowAnimated;
+    private final IDrawableStatic flame;
+    private final IDrawableAnimated flameAnimated;
+
     public PyrolysisRecipeCategory(IGuiHelper helper) {
         super(helper, JEIRecipeTypes.PYROLYSIS, "jei.gasworks.pyrolysis");
         setIcon(helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, GSItems.CHARCOAL_PILE.toStack()));
+        // Setup for the texture parts drawn in JEI
+        arrow = helper.drawableBuilder(guiElements, 16, 0, 24, 16).setTextureSize(48, 48).build();
+        arrowAnimated = helper.drawableBuilder(guiElements, 16, 16, 24, 16).setTextureSize(48, 48).buildAnimated(160, IDrawableAnimated.StartDirection.LEFT, false);
+        flame = helper.drawableBuilder(guiElements, 0, 0, 16, 16).setTextureSize(48, 48).build();
+        flameAnimated = helper.drawableBuilder(guiElements, 0, 16, 16, 16).setTextureSize(48, 48).buildAnimated(160, IDrawableAnimated.StartDirection.TOP, true);
     }
 
     @Override
     public int getWidth() {
-        return 142;
+        return 122;
     }
 
     @Override
@@ -43,10 +58,10 @@ public class PyrolysisRecipeCategory extends RecipeCategory<PyrolysisRecipe> {
         builder.addSlot(RecipeIngredientRole.INPUT, 21, 3)
             .addItemStack(recipe.getIngredient());
 
-        builder.addSlot(RecipeIngredientRole.OUTPUT, 105, 3)
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 85, 3)
             .addItemStack(recipe.getResult());
 
-        builder.addSlot(RecipeIngredientRole.OUTPUT, 125, 3)
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 105, 3)
             .addItemStack(recipe.getAsh());
     }
 
@@ -55,14 +70,12 @@ public class PyrolysisRecipeCategory extends RecipeCategory<PyrolysisRecipe> {
         // Draw the slots
         JEIPlugin.slotDrawable.draw(graphics, 0, 2);
         JEIPlugin.slotDrawable.draw(graphics, 20, 2);
+        JEIPlugin.slotDrawable.draw(graphics, 84, 2);
         JEIPlugin.slotDrawable.draw(graphics, 104, 2);
-        JEIPlugin.slotDrawable.draw(graphics, 124, 2);
         // Draw the moving parts
-        /*
-        this.flame.draw(graphics, 52, 0);
-        this.flameAnimated.draw(graphics, 52, 0);
-        this.arrow.draw(graphics, 50, 16);
-        this.arrowAnimated.draw(graphics, 50, 16);
-         */
+        this.flame.draw(graphics, 39, 3);
+        this.flameAnimated.draw(graphics, 39, 3);
+        this.arrow.draw(graphics, 57, 3);
+        this.arrowAnimated.draw(graphics, 57, 3);
     }
 }
