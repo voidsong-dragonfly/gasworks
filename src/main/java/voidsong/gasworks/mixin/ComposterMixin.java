d4b18a03f81cdf7a92e35c8c70b28afc1c15399e
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2025-05-26 16:04:24 -0400

    Switch composter from making bonemeal to compost piles

---
+++ src/main/java/voidsong/gasworks/mixin/ComposterMixin.java
@@ -0,0 +1,36 @@
+package voidsong.gasworks.mixin;
+
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.WorldlyContainer;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.ComposterBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import voidsong.gasworks.common.registry.GSItems;
+
+@SuppressWarnings("unused")
+@Mixin(ComposterBlock.class)
+public abstract class ComposterMixin {
+
+    @Mixin(ComposterBlock.OutputContainer.class)
+    abstract static class OutputContainer extends SimpleContainer implements WorldlyContainer {
+        @ModifyArg(method = "canTakeItemThroughFace", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"), index = 0)
+        private Item isCorrectItem(Item original) {
+            return original.equals(Items.BONE_MEAL) ? GSItems.COMPOST_PILE.get() : original;
+        }
+    }
+
+    @ModifyArg(method = "extractProduce", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;<init>(Lnet/minecraft/world/level/ItemLike;)V"), index = 0)
+    private static ItemLike extractCorrectItem(ItemLike original) {
+        return original.equals(Items.BONE_MEAL) ? GSItems.COMPOST_PILE.get() : original;
+    }
+
+    @ModifyArg(method = "getContainer", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;<init>(Lnet/minecraft/world/level/ItemLike;)V"), index = 0)
+    private ItemLike containerCorrectItem(ItemLike original) {
+        return original.equals(Items.BONE_MEAL) ? GSItems.COMPOST_PILE.get() : original;
+    }
+}
+
