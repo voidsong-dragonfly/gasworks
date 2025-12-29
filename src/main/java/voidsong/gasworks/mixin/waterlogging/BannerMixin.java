08cf424dd3917521efb25bfd1d0f700a4b8ebd0f
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2025-12-29 14:28:28 -0500

    First round of new blocks for waterlogging
     - Added doors, pressure plates, end rods, fence gates, and both placement types of banners

---
+++ src/main/java/voidsong/gasworks/mixin/waterlogging/BannerMixin.java
@@ -0,0 +1,80 @@
+package voidsong.gasworks.mixin.waterlogging;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.BannerBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import javax.annotation.Nonnull;
+
+@Mixin(BannerBlock.class)
+public class BannerMixin extends Block implements SimpleWaterloggedBlock {
+    /**
+     * This constructor is the default & will be ignored, it exists so we can extend Block
+     * @param properties ignored & should not be used!
+     */
+    public BannerMixin(Properties properties) {
+        super(properties);
+    }
+
+    @ModifyArg(method = "<init>(Lnet/minecraft/world/item/DyeColor;Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/BannerBlock;registerDefaultState(Lnet/minecraft/world/level/block/state/BlockState;)V"), index = 0)
+    private BlockState addWaterloggingToConstructor(BlockState defaultState) {
+        return defaultState.setValue(BlockStateProperties.WATERLOGGED, false);
+    }
+
+    @Inject(method = "createBlockStateDefinition", at = @At(value = "RETURN"))
+    private void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder, CallbackInfo ci) {
+        builder.add(BlockStateProperties.WATERLOGGED);
+    }
+
+    /*
+     * The methods below were copied & modified from CandleBlock, to provide waterlogging capabilities to other
+     * blocks. These are generalized to remove references to lit & only add state-dependent waterlogging behavior
+     */
+
+    @ModifyReturnValue(method = "getStateForPlacement", at = @At(value = "RETURN"))
+    private BlockState getStateForPlacement(BlockState toPlace, @Local(argsOnly = true) BlockPlaceContext context) {
+        FluidState fluid = context.getLevel().getFluidState(context.getClickedPos());
+        boolean waterlogged = fluid.getType() == Fluids.WATER;
+        return toPlace == null ? null : toPlace.setValue(BlockStateProperties.WATERLOGGED, waterlogged);
+    }
+
+    @ModifyReturnValue(method = "updateShape", at = @At(value = "RETURN"))
+    private BlockState updateShape(BlockState updated, @Local(argsOnly = true) LevelAccessor level, @Local(ordinal = 0, argsOnly = true) BlockPos pos) {
+        if (updated.hasProperty(BlockStateProperties.WATERLOGGED) && updated.getValue(BlockStateProperties.WATERLOGGED))
+            level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+        return updated;
+    }
+
+    @Override
+    @Nonnull
+    protected FluidState getFluidState(BlockState state) {
+        return state.getValue(BlockStateProperties.WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    @Override
+    public boolean placeLiquid(@Nonnull LevelAccessor level, @Nonnull BlockPos pos, BlockState state, @Nonnull FluidState fluidState) {
+        if (!state.getValue(BlockStateProperties.WATERLOGGED) && fluidState.getType() == Fluids.WATER) {
+            BlockState blockstate = state.setValue(BlockStateProperties.WATERLOGGED, true);
+            level.setBlock(pos, blockstate, 3);
+            level.scheduleTick(pos, fluidState.getType(), fluidState.getType().getTickDelay(level));
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/voidsong/gasworks/mixin/waterlogging/EndRodMixin.java
@@ -0,0 +1,106 @@
+package voidsong.gasworks.mixin.waterlogging;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EndRodBlock;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import voidsong.gasworks.mixin.accessor.FlowingFluidAccessor;
+
+import javax.annotation.Nonnull;
+
+@Mixin(EndRodBlock.class)
+public class EndRodMixin extends Block implements SimpleWaterloggedBlock {
+    /**
+     * This constructor is the default & will be ignored, it exists so we can extend Block
+     * @param properties ignored & should not be used!
+     */
+    public EndRodMixin(Properties properties) {
+        super(properties);
+    }
+
+    @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/EndRodBlock;registerDefaultState(Lnet/minecraft/world/level/block/state/BlockState;)V"), index = 0)
+    private BlockState addWaterloggingToConstructor(BlockState defaultState) {
+        return defaultState.setValue(BlockStateProperties.WATERLOGGED, false);
+    }
+
+    @Inject(method = "createBlockStateDefinition", at = @At(value = "RETURN"))
+    private void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder, CallbackInfo ci) {
+        builder.add(BlockStateProperties.WATERLOGGED);
+    }
+
+    /*
+     * The methods below were copied & modified from CandleBlock, to provide waterlogging capabilities to other
+     * blocks. These are generalized to remove references to lit & only add state-dependent waterlogging behavior
+     */
+
+    @ModifyReturnValue(method = "getStateForPlacement", at = @At(value = "RETURN"))
+    private BlockState getStateForPlacement(BlockState toPlace, @Local(argsOnly = true) BlockPlaceContext context) {
+        FluidState fluid = context.getLevel().getFluidState(context.getClickedPos());
+        boolean waterlogged = fluid.getType() == Fluids.WATER;
+        return toPlace == null ? null : toPlace.setValue(BlockStateProperties.WATERLOGGED, waterlogged);
+    }
+
+    @Override
+    @Nonnull
+    protected BlockState updateShape(@Nonnull BlockState updated, @Nonnull Direction direction, @Nonnull BlockState neighborState, @Nonnull LevelAccessor level, @Nonnull BlockPos pos, @Nonnull BlockPos neighborPos) {
+        if (updated.getValue(BlockStateProperties.WATERLOGGED))
+            level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+        return super.updateShape(updated, direction, neighborState, level, pos, neighborPos);
+    }
+
+    @Override
+    @Nonnull
+    protected FluidState getFluidState(BlockState state) {
+        return state.getValue(BlockStateProperties.WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    @Override
+    public boolean placeLiquid(@Nonnull LevelAccessor level, @Nonnull BlockPos pos, BlockState state, @Nonnull FluidState fluidState) {
+        if (!state.getValue(BlockStateProperties.WATERLOGGED)) {
+            if (fluidState.getType() == Fluids.WATER) {
+                BlockState blockstate = state.setValue(BlockStateProperties.WATERLOGGED, true);
+                level.setBlock(pos, blockstate, 3);
+                level.scheduleTick(pos, fluidState.getType(), fluidState.getType().getTickDelay(level));
+                // These exceptions to the placeLiquid code from SimpleWaterloggedBlock exists explicitly to allow torches
+                // to be washed away, while also containing waterlogging, it is copied from FlowingFluid#spreadTo
+            } else if (fluidState.getType() instanceof FlowingFluidAccessor flowingFluid) {
+                flowingFluid.callBeforeDestroyingBlock(level, pos, state);
+                level.setBlock(pos, fluidState.createLegacyBlock(), 3);
+            } else {
+                level.setBlock(pos, fluidState.createLegacyBlock(), 3);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+     * The method below is modified from SimpleWaterloggedBlock & LiquidBlockContainer to allow torches to still be
+     * washed away by flowing water, while also containing waterlogging
+     */
+
+    @Override
+    public boolean canPlaceLiquid(@Nullable Player player, @Nonnull BlockGetter level, @Nonnull BlockPos pos, @Nonnull BlockState state, @Nonnull Fluid fluid) {
+        return true;
+    }
+}
